ee536e0364f799de5986ad6e50499e4beb8eaa36
Florian <florian.robert.3@etu.u-bordeaux.fr>
2018-09-18 09:24:15 +0200

    Modification du programme pour tracer le graphe logarithmique

--- tp1/src/programme.py
+++ tp1/src/programme.py
@@ -7,28 +7,38 @@ import matplotlib.pyplot as plt
 import methodes
 import equations
 
-# Résolution approchée de y' = 2-y avec y(0) = 3 pour t dans [0,5]
+# Résolution approchée de y' = 1-y avec y(0) = 5 pour t dans [0,1]
 equations.a = -1.
-equations.b = 2.
-t0,y0 = 0.,3.
-T = 5.
-# Avec un pas de 0.1, il faut donc 50 iterations
-N,h = 50,0.1
-[t,y1] = methodes.euler_explicite(t0,h,N,y0,equations.f_affine)
-# Solution exacte aux mêmes instants
-z1 = equations.sol_affine(t,y0)
-# Calcul de l'erreur maximum relative
-e1 = np.max(np.abs((z1-y1)/z1))
-# Graphe des solutions exactes et approchées
-plt.plot(t,y1,'b-+',label='solution approchée h=0.1')
-plt.plot(t,z1,'r-',label='solution exacte')
-plt.xlabel('t')
-plt.ylabel('y')
-plt.title("Méthode d'Euler explicite")
+equations.b = 1.
+t0,y0 = 0.,5.
+T = 1.
+# Avec un pas de 0.2, il faut donc 5 iterations
+h = [0.2, 0.1, 0.05, 0.025]
+Err = []
+for i in range(len(h)):
+    N=int(T/h[i]) #recalcul du nombre d'itérations par pas
+    [t,y1] = methodes.euler_explicite(t0,h[i],N,y0,equations.f_affine)
+    # Solution exacte aux mêmes instants
+    z1 = equations.sol_affine(t,y0)
+    # Calcul de l'erreur maximum relative
+    e1 = np.max(np.abs((z1-y1)/z1))
+    Err.append(e1)
+    # Affichage et sauvegarde du graphe des solutions exactes et approchées
+    plt.plot(t,y1,'b-+',label='solution approchée h=0.1')
+    plt.plot(t,z1,'r-',label='solution exacte')
+    plt.xlabel('t')
+    plt.ylabel('y')
+    plt.title("Méthode d'Euler explicite")
+    plt.legend()
+    plt.show()
+    plt.savefig('euler_explicite_%2u.png'.format(i))
+
+# Écriture de l'erreur en fonction de h et impression du graphique
+print("{0} | {1}".format(h,Err))
+plt.loglog(h,Err)
+plt.xlabel('h')
+plt.ylabel('Err')
+plt.title('Err = f(h) (logarithmique)')
 plt.legend()
-plt.savefig('eq_affine.png')
-
-# Écriture de l'erreur en fonction de h
-print("{0} | {1}".format(h,e1))
-
-
+plt.show()
+plt.savefig('Err_fct_h.png')
